e84d7641393ab5a34edd4ceccc492f3015c09702
Chantel <cherry200805092022@163.com>
2026-09-09 18:30:32 +0800

    Easy: 66. Plus One | Approach 1 | Runtime: 0 ms | Memory: 19412000

---
+++ 66. Plus One/Approach_1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n=len(digits)-1
+        if digits[n]!=9:
+            digits[n]+=1
+        else:
+            digits[n]=0
+            n-=1
+            while n>=0 and digits[n]==9:
+                digits[n]=0
+                n-=1
+            if n<0:
+                digits.insert(0,1)
+            else:
+                digits[n]+=1
+        return digits
